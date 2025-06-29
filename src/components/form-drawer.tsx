1cbcbfe9b972c2d4f8443ed44e4fd3573dc318fb
TingLong8912 <cljh303@gmail.com>
2025-06-29 19:58:36 +0800

    update drawer

--- src/components/form-drawer.tsx
+++ src/components/form-drawer.tsx
@@ -6,9 +6,9 @@ import { Badge } from "@/components/ui/badge"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
-import { 
-  ChevronsUp, 
-  ChevronsDown, 
+import {
+  ChevronsUp,
+  ChevronsDown,
   Search
 
 } from "lucide-react";
@@ -27,6 +27,14 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import {
+  Download,
+  Flower2,
+  Map,
+  Book,
+  Hand,
+  LayoutList
+} from "lucide-react"
 
 export function FormDrawer() {
   const [isOpen, setIsOpen] = useState(false);
@@ -34,83 +42,94 @@ export function FormDrawer() {
   return (
     <Drawer open={isOpen} onOpenChange={setIsOpen}>
       <DrawerTrigger asChild>
-        <Button 
-          variant="ghost" size="icon" 
+        <Button
+          variant="ghost" size="icon"
           className="md:hidden focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
           onClick={() => setIsOpen(true)}
         >
-          {isOpen ? 
-            <ChevronsDown className="size-4"/>
-            : <ChevronsUp className="size-4"/>
+          {isOpen ?
+            <ChevronsDown className="size-4" />
+            : <ChevronsUp className="size-4" />
           }
         </Button>
       </DrawerTrigger>
       <DrawerContent className="max-h-[80vh]">
         <DrawerHeader>
-          <DrawerTitle>Configuration</DrawerTitle>
+          <DrawerTitle>檢索</DrawerTitle>
           <DrawerDescription>
-            Configure the settings for the model and messages.
+
           </DrawerDescription>
         </DrawerHeader>
-        <form className="grid w-full items-start gap-6 overflow-auto p-4 pt-0">
+
+        <form className="grid w-full items-start gap-6">
           <fieldset className="grid gap-6 rounded-lg border p-4">
-            <legend className="-ml-1 px-1 text-sm font-medium">
-              <Search className="size-3.5" />
-            </legend>
             <div className="grid gap-3">
-              <Label htmlFor="model">檢索欄位</Label>
+              <Label htmlFor="search">快速搜尋</Label>
+              <Input
+                id="search"
+                placeholder="請輸入中藥名稱"
+              />
+            </div>
+          </fieldset>
+          <fieldset className="grid gap-6 rounded-lg border p-4">
+            <div className="grid gap-3">
+              <Label htmlFor="role">欄位檢索</Label>
               <Select>
                 <SelectTrigger
                   id="model"
                   className="items-start [&_[data-description]]:hidden"
                 >
-                  <SelectValue placeholder="Select a model" />
+                  <SelectValue placeholder="選擇檢索類別" />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="genesis">
+                  <SelectItem value="plant_part">
                     <div className="flex items-start gap-3 text-muted-foreground">
-                      {/* <Rabbit className="size-5" /> */}
+                      <Flower2 className="size-5" />
                       <div className="grid gap-0.5">
-                        <p>
-                          Neural{" "}
-                          <span className="font-medium text-foreground">
-                            Genesis
-                          </span>
+                        <p className="font-medium text-foreground">
+                          植物部分
                         </p>
                         <p className="text-xs" data-description>
-                          Our fastest model for general use cases.
+                          種子、莖、葉、花等等
                         </p>
                       </div>
                     </div>
                   </SelectItem>
-                  <SelectItem value="explorer">
+                  <SelectItem value="region">
                     <div className="flex items-start gap-3 text-muted-foreground">
-                      {/* <Bird className="size-5" /> */}
+                      <Map className="size-5" />
                       <div className="grid gap-0.5">
-                        <p>
-                          Neural{" "}
-                          <span className="font-medium text-foreground">
-                            Explorer
-                          </span>
+                        <p className="font-medium text-foreground">
+                          地點
                         </p>
                         <p className="text-xs" data-description>
-                          Performance and speed for efficiency.
+                          廣東、廣西等等
                         </p>
                       </div>
                     </div>
                   </SelectItem>
-                  <SelectItem value="quantum">
+                  <SelectItem value="provenance">
                     <div className="flex items-start gap-3 text-muted-foreground">
+                      <Book className="size-5" />
                       <div className="grid gap-0.5">
-                        <p>
-                          Neural{" "}
-                          <span className="font-medium text-foreground">
-                            Quantum
-                          </span>
+                        <p className="font-medium text-foreground">
+                          出處
                         </p>
                         <p className="text-xs" data-description>
-                          The most powerful model for complex
-                          computations.
+                          本草綱目拾遺、中藥大辭典等等
+                        </p>
+                      </div>
+                    </div>
+                  </SelectItem>
+                  <SelectItem value="usage">
+                    <div className="flex items-start gap-3 text-muted-foreground">
+                      <Hand className="size-5" />
+                      <div className="grid gap-0.5">
+                        <p className="font-medium text-foreground">
+                          外觀及功用
+                        </p>
+                        <p className="text-xs" data-description>
+                          苦、澀、治感冒等等
                         </p>
                       </div>
                     </div>
@@ -119,40 +138,18 @@ export function FormDrawer() {
               </Select>
             </div>
             <div className="grid gap-3">
-              <Label htmlFor="temperature">Temperature</Label>
-              <Input id="temperature" type="number" placeholder="0.4" />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="top-p">Top P</Label>
-              <Input id="top-p" type="number" placeholder="0.7" />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="top-k">Top K</Label>
-              <Input id="top-k" type="number" placeholder="0.0" />
-            </div>
-          </fieldset>
-          <fieldset className="grid gap-6 rounded-lg border p-4">
-            <legend className="-ml-1 px-1 text-sm font-medium">
-            </legend>
-            <div className="grid gap-3">
-              <Label htmlFor="role">Role</Label>
-              <Select defaultValue="system">
-                <SelectTrigger>
-                  <SelectValue placeholder="Select a role" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="system">System</SelectItem>
-                  <SelectItem value="user">User</SelectItem>
-                  <SelectItem value="assistant">Assistant</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="content">Content</Label>
-              <Textarea id="content" placeholder="You are a..." />
+              <Input
+                id="search_keywords"
+                placeholder="請輸入欲查詢之關鍵字"
+              />
             </div>
           </fieldset>
         </form>
+        <fieldset className="grid gap-6 rounded-lg border p-4">
+          <div className="grid gap-3">
+            <Label htmlFor="lists">列表檢索</Label>
+          </div>
+        </fieldset>
       </DrawerContent>
     </Drawer>
   );
